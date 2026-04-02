6e031e249f9ee799e5a4db09296b57b0bd8d6d96
calmilamsy <bumbill00@gmail.com>
2026-04-02 23:09:45 +0100

    Dunk basic items that I have textures for in

--- src/main/java/net/glasslauncher/mods/glasstech/events/init/ClientInitListener.java
+++ src/main/java/net/glasslauncher/mods/glasstech/events/init/ClientInitListener.java
@@ -39,6 +39,23 @@ public class ClientInitListener {
 
     public static Atlas.Sprite energySlotIndex = null;
 
+    public static Atlas.Sprite emptyBattery;
+    public static Atlas.Sprite emptyEnergyCrystal;
+    public static Atlas.Sprite emptyLapotronCrystal;
+    public static Atlas.Sprite emptyBatteryPack;
+    public static Atlas.Sprite almostEmptyBattery;
+    public static Atlas.Sprite almostEmptyEnergyCrystal;
+    public static Atlas.Sprite almostEmptyLapotronCrystal;
+    public static Atlas.Sprite almostEmptyBatteryPack;
+    public static Atlas.Sprite almostFullBattery;
+    public static Atlas.Sprite almostFullEnergyCrystal;
+    public static Atlas.Sprite almostFullLapotronCrystal;
+    public static Atlas.Sprite almostFullBatteryPack;
+    public static Atlas.Sprite fullBattery;
+    public static Atlas.Sprite fullEnergyCrystal;
+    public static Atlas.Sprite fullLapotronCrystal;
+    public static Atlas.Sprite fullBatteryPack;
+
     @EventListener
     private static void screenInit(GuiHandlerRegistryEvent event) {
         event.register(NAMESPACE.id("generator"), new GuiHandler((player, inventory, packet) -> new GeneratorScreen(player.inventory, (GeneratorBlockEntity) inventory), GeneratorBlockEntity::new));
@@ -53,5 +70,66 @@ public class ClientInitListener {
     @EventListener
     private static void textureInit(TextureRegisterEvent event) {
         energySlotIndex = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/battery_slot"));
+
+        InitListener.copperIngot.setTexture(NAMESPACE.id("item/resource/copper_ingot"));
+        InitListener.advancedAlloy.setTexture(NAMESPACE.id("item/resource/advanced_alloy"));
+        InitListener.scrap.setTexture(NAMESPACE.id("item/resource/scrap"));
+        InitListener.copperDust.setTexture(NAMESPACE.id("item/resource/copper_dust"));
+        InitListener.tinIngot.setTexture(NAMESPACE.id("item/resource/tin_ingot"));
+        InitListener.tinDust.setTexture(NAMESPACE.id("item/resource/tin_dust"));
+        InitListener.bronzeIngot.setTexture(NAMESPACE.id("item/resource/bronze_ingot"));
+        InitListener.bronzeDust.setTexture(NAMESPACE.id("item/resource/bronze_dust"));
+        InitListener.goldDust.setTexture(NAMESPACE.id("item/resource/gold_dust"));
+        InitListener.ironDust.setTexture(NAMESPACE.id("item/resource/iron_dust"));
+        InitListener.iridium.setTexture(NAMESPACE.id("item/resource/iridium_ore"));
+        InitListener.iridiumPlate.setTexture(NAMESPACE.id("item/resource/iridium_plate"));
+        InitListener.uraniumIngot.setTexture(NAMESPACE.id("item/resource/uranium_ingot"));
+        InitListener.rubber.setTexture(NAMESPACE.id("item/resource/rubber"));
+        InitListener.resin.setTexture(NAMESPACE.id("item/resource/resin"));
+        InitListener.emptyCell.setTexture(NAMESPACE.id("item/resource/empty_cell"));
+        InitListener.lavaCell.setTexture(NAMESPACE.id("item/resource/lava_cell"));
+        InitListener.waterCell.setTexture(NAMESPACE.id("item/resource/water_cell"));
+        InitListener.mixedMetalIngot.setTexture(NAMESPACE.id("item/resource/mixed_metal_ingot"));
+        InitListener.refinedIronIngot.setTexture(NAMESPACE.id("item/resource/refined_iron_ingot"));
+        InitListener.uranium.setTexture(NAMESPACE.id("item/resource/uranium_ore"));
+        InitListener.uuMatter.setTexture(NAMESPACE.id("item/resource/uu_matter"));
+        InitListener.coalDust.setTexture(NAMESPACE.id("item/resource/coal_dust"));
+        InitListener.fertilizer.setTexture(NAMESPACE.id("item/resource/fertilizer"));
+
+        InitListener.circuit.setTexture(NAMESPACE.id("item/electronic_circuit"));
+        InitListener.advancedCircuit.setTexture(NAMESPACE.id("item/advanced_circuit"));
+        InitListener.scrapBox.setTexture(NAMESPACE.id("item/scrap_box"));
+
+        emptyBattery = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/empty_battery"));
+        emptyEnergyCrystal = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/empty_energy_crystal"));
+        emptyLapotronCrystal = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/empty_lapotron_crystal"));
+        emptyBatteryPack = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/empty_battery_pack"));
+
+        InitListener.reBattery.setTextureId(emptyBattery.index);
+        InitListener.energyCrystal.setTextureId(emptyEnergyCrystal.index);
+        InitListener.lapotronCrystal.setTextureId(emptyLapotronCrystal.index);
+        InitListener.batteryPack.setTextureId(emptyBatteryPack.index);
+
+        almostEmptyBattery = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/almost_empty_battery"));
+        almostEmptyEnergyCrystal = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/almost_empty_energy_crystal"));
+        almostEmptyLapotronCrystal = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/almost_empty_lapotron_crystal"));
+        almostEmptyBatteryPack = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/almost_empty_battery_pack"));
+
+        almostFullBattery = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/almost_full_battery"));
+        almostFullEnergyCrystal = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/almost_full_energy_crystal"));
+        almostFullLapotronCrystal = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/almost_full_lapotron_crystal"));
+        almostFullBatteryPack = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/almost_full_battery_pack"));
+
+        fullBattery = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/full_battery"));
+        fullEnergyCrystal = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/full_energy_crystal"));
+        fullLapotronCrystal = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/full_lapotron_crystal"));
+        fullBatteryPack = Atlases.getGuiItems().addTexture(NAMESPACE.id("item/storage/full_battery_pack"));
+
+        InitListener.wrench.setTexture(NAMESPACE.id("item/tool/wrench"));
+        InitListener.electricWrench.setTexture(NAMESPACE.id("item/tool/electric_wrench"));
+        InitListener.chainsaw.setTexture(NAMESPACE.id("item/tool/chainsaw"));
+        InitListener.diamondChainsaw.setTexture(NAMESPACE.id("item/tool/diamond_chainsaw"));
+        InitListener.drill.setTexture(NAMESPACE.id("item/tool/drill"));
+        InitListener.diamondDrill.setTexture(NAMESPACE.id("item/tool/diamond_drill"));
     }
 }
--- src/main/java/net/glasslauncher/mods/glasstech/events/init/InitListener.java
+++ src/main/java/net/glasslauncher/mods/glasstech/events/init/InitListener.java
@@ -20,10 +20,13 @@ import net.glasslauncher.mods.glasstech.blocks.machine.macerator.MaceratorBlockE
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 import net.modificationstation.stationapi.api.event.block.entity.BlockEntityRegisterEvent;
 import net.modificationstation.stationapi.api.event.mod.InitEvent;
 import net.modificationstation.stationapi.api.event.registry.BlockRegistryEvent;
+import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.EntrypointManager;
+import net.modificationstation.stationapi.api.template.item.TemplateItem;
 import net.modificationstation.stationapi.api.util.Namespace;
 import org.apache.logging.log4j.Logger;
 
@@ -51,6 +54,47 @@ public class InitListener {
 
     public static Block copperCableBlock;
     public static Block insulatedCopperCableBlock;
+
+    public static Item copperIngot;
+    public static Item advancedAlloy;
+    public static Item scrap;
+    public static Item copperDust;
+    public static Item tinIngot;
+    public static Item tinDust;
+    public static Item bronzeIngot;
+    public static Item bronzeDust;
+    public static Item goldDust;
+    public static Item ironDust;
+    public static Item iridium;
+    public static Item iridiumPlate;
+    public static Item uraniumIngot;
+    public static Item rubber;
+    public static Item resin;
+    public static Item emptyCell;
+    public static Item lavaCell;
+    public static Item waterCell;
+    public static Item mixedMetalIngot;
+    public static Item refinedIronIngot;
+    public static Item uranium;
+    public static Item uuMatter;
+    public static Item coalDust;
+    public static Item fertilizer;
+
+    public static Item circuit;
+    public static Item advancedCircuit;
+    public static Item scrapBox;
+
+    public static Item reBattery;
+    public static Item energyCrystal;
+    public static Item lapotronCrystal;
+    public static Item batteryPack;
+
+    public static Item wrench;
+    public static Item electricWrench;
+    public static Item chainsaw;
+    public static Item diamondChainsaw;
+    public static Item drill;
+    public static Item diamondDrill;
 
     @EventListener
     private static void init(InitEvent event) {
@@ -81,5 +125,49 @@ public class InitListener {
         event.register(CannerBlockEntity.class, NAMESPACE.id("canner").toString());
         event.register(ElectrolyzerBlockEntity.class, NAMESPACE.id("electrolyzer").toString());
         event.register(ExtractorBlockEntity.class, NAMESPACE.id("extractor").toString());
+    }
+
+    @EventListener
+    private static void itemInit(ItemRegistryEvent event) {
+        copperIngot = new TemplateItem(NAMESPACE.id("copper_ingot")).setTranslationKey(NAMESPACE.id("copper_ingot"));
+        advancedAlloy = new TemplateItem(NAMESPACE.id("advanced_alloy")).setTranslationKey(NAMESPACE.id("advanced_alloy"));
+        scrap = new TemplateItem(NAMESPACE.id("scrap")).setTranslationKey(NAMESPACE.id("scrap"));
+        copperDust = new TemplateItem(NAMESPACE.id("copper_dust")).setTranslationKey(NAMESPACE.id("copper_dust"));
+        tinIngot = new TemplateItem(NAMESPACE.id("tin_ingot")).setTranslationKey(NAMESPACE.id("tin_ingot"));
+        tinDust = new TemplateItem(NAMESPACE.id("tin_dust")).setTranslationKey(NAMESPACE.id("tin_dust"));
+        bronzeIngot = new TemplateItem(NAMESPACE.id("bronze_ingot")).setTranslationKey(NAMESPACE.id("bronze_ingot"));
+        bronzeDust = new TemplateItem(NAMESPACE.id("bronze_dust")).setTranslationKey(NAMESPACE.id("bronze_dust"));
+        goldDust = new TemplateItem(NAMESPACE.id("gold_dust")).setTranslationKey(NAMESPACE.id("gold_dust"));
+        ironDust = new TemplateItem(NAMESPACE.id("iron_dust")).setTranslationKey(NAMESPACE.id("iron_dust"));
+        iridium = new TemplateItem(NAMESPACE.id("iridium")).setTranslationKey(NAMESPACE.id("iridium"));
+        iridiumPlate = new TemplateItem(NAMESPACE.id("iridium_plate")).setTranslationKey(NAMESPACE.id("iridium_plate"));
+        uraniumIngot = new TemplateItem(NAMESPACE.id("uranium_ingot")).setTranslationKey(NAMESPACE.id("uranium_ingot"));
+        rubber = new TemplateItem(NAMESPACE.id("rubber")).setTranslationKey(NAMESPACE.id("rubber"));
+        resin = new TemplateItem(NAMESPACE.id("resin")).setTranslationKey(NAMESPACE.id("resin"));
+        emptyCell = new TemplateItem(NAMESPACE.id("empty_cell")).setTranslationKey(NAMESPACE.id("empty_cell"));
+        lavaCell = new TemplateItem(NAMESPACE.id("lava_cell")).setTranslationKey(NAMESPACE.id("lava_cell"));
+        waterCell = new TemplateItem(NAMESPACE.id("water_cell")).setTranslationKey(NAMESPACE.id("water_cell"));
+        mixedMetalIngot = new TemplateItem(NAMESPACE.id("mixed_metal_ingot")).setTranslationKey(NAMESPACE.id("mixed_metal_ingot"));
+        refinedIronIngot = new TemplateItem(NAMESPACE.id("refined_iron_ingot")).setTranslationKey(NAMESPACE.id("refined_iron_ingot"));
+        uranium = new TemplateItem(NAMESPACE.id("uranium")).setTranslationKey(NAMESPACE.id("uranium"));
+        uuMatter = new TemplateItem(NAMESPACE.id("uu_matter")).setTranslationKey(NAMESPACE.id("uu_matter"));
+        coalDust = new TemplateItem(NAMESPACE.id("coal_dust")).setTranslationKey(NAMESPACE.id("coal_dust"));
+        fertilizer = new TemplateItem(NAMESPACE.id("fertilizer")).setTranslationKey(NAMESPACE.id("fertilizer"));
+
+        circuit = new TemplateItem(NAMESPACE.id("circuit")).setTranslationKey(NAMESPACE.id("circuit"));
+        advancedCircuit = new TemplateItem(NAMESPACE.id("advanced_circuit")).setTranslationKey(NAMESPACE.id("advanced_circuit"));
+        scrapBox = new TemplateItem(NAMESPACE.id("scrap_box")).setTranslationKey(NAMESPACE.id("scrap_box"));
+
+        reBattery = new TemplateItem(NAMESPACE.id("re_battery")).setTranslationKey(NAMESPACE.id("re_battery"));
+        energyCrystal = new TemplateItem(NAMESPACE.id("energy_crystal")).setTranslationKey(NAMESPACE.id("energy_crystal"));
+        lapotronCrystal = new TemplateItem(NAMESPACE.id("lapotron_crystal")).setTranslationKey(NAMESPACE.id("lapotron_crystal"));
+        batteryPack = new TemplateItem(NAMESPACE.id("battery_pack")).setTranslationKey(NAMESPACE.id("battery_pack"));
+
+        wrench = new TemplateItem(NAMESPACE.id("wrench")).setTranslationKey(NAMESPACE.id("wrench"));
+        electricWrench = new TemplateItem(NAMESPACE.id("electric_wrench")).setTranslationKey(NAMESPACE.id("electric_wrench"));
+        chainsaw = new TemplateItem(NAMESPACE.id("chainsaw")).setTranslationKey(NAMESPACE.id("chainsaw"));
+        diamondChainsaw = new TemplateItem(NAMESPACE.id("diamond_chainsaw")).setTranslationKey(NAMESPACE.id("diamond_chainsaw"));
+        drill = new TemplateItem(NAMESPACE.id("drill")).setTranslationKey(NAMESPACE.id("drill"));
+        diamondDrill = new TemplateItem(NAMESPACE.id("diamond_drill")).setTranslationKey(NAMESPACE.id("diamond_drill"));
     }
 }
