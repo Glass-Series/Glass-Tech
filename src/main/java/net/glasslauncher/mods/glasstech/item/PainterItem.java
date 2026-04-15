ab4888b33556a2f78a0213f4f8538db9e82b0b90
calmilamsy <bumbill00@gmail.com>
2026-04-15 14:38:46 +0100

    And this is my functional painter code, EXCEPT IT ISN'T

---
+++ src/main/java/net/glasslauncher/mods/glasstech/item/PainterItem.java
@@ -0,0 +1,76 @@
+package net.glasslauncher.mods.glasstech.item;
+
+import net.glasslauncher.mods.glasstech.blocks.GTHardenedFoamBlockColorProvider;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.World;
+import net.modificationstation.stationapi.api.util.Identifier;
+import net.modificationstation.stationapi.api.block.BlockState;
+import net.modificationstation.stationapi.api.template.item.TemplateItem;
+
+import static net.glasslauncher.mods.glasstech.GlassTech.LOGGER;
+
+public class PainterItem extends TemplateItem {
+
+    public PainterItem(Identifier identifier) {
+        super(identifier);
+        setMaxDamage(129);
+        setMaxCount(1);
+    }
+
+    @Override
+    public boolean isDamageable() {
+        return true;
+    }
+
+    @Override
+    public boolean useOnBlock(ItemStack stack, PlayerEntity player, World world, int x, int y, int z, int side) {
+        if (world.isRemote) {
+            return true;
+        }
+
+        BlockState state = world.getBlockState(x, y, z);
+        NbtCompound nbt = stack.getStationNbt();
+        String color = nbt.getString("color");
+
+        if (color == null || color.isEmpty()) {
+            return false;
+        }
+
+        GTHardenedFoamBlockColorProvider.FoamColor foamColor;
+        try {
+            foamColor = GTHardenedFoamBlockColorProvider.FoamColor.valueOf(color);
+        } catch (Exception e) {
+            LOGGER.error("Painter has invalid color?", e);
+            foamColor = null;
+        }
+
+        if (foamColor == null) {
+            return false;
+        }
+
+        if (state.contains(GTHardenedFoamBlockColorProvider.FOAM_COLOR_PROPERTY)) {
+            state = state.with(GTHardenedFoamBlockColorProvider.FOAM_COLOR_PROPERTY, foamColor);
+            world.setBlockStateWithNotify(x, y, z, state);
+            world.playSound(player, "random.click", 0.5f, 1);
+            stack.damage(1, null);
+            if (stack.getDamage() == stack.getMaxDamage() - 1) {
+                nbt.putString("color", null);
+            }
+            return true;
+        }
+        else if (state.getBlock() == Block.WOOL) {
+            world.setBlockMeta(x, y, z, foamColor.meta);
+            world.playSound(player, "random.click", 0.5f, 1);
+            stack.damage(1, null);
+            if (stack.getDamage() == stack.getMaxDamage() - 1) {
+                nbt.entries.remove("color");
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/net/glasslauncher/mods/glasstech/mixin/CraftingResultSlotMixin.java
@@ -0,0 +1,33 @@
+package net.glasslauncher.mods.glasstech.mixin;
+
+import net.glasslauncher.mods.glasstech.events.init.RecipeInit;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.slot.CraftingResultSlot;
+import net.modificationstation.stationapi.api.StationAPI;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CraftingResultSlot.class)
+public class CraftingResultSlotMixin {
+
+    @Shadow
+    @Final
+    private Inventory input;
+
+    @Inject(
+            method = "onTakeItem",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;onCraft(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;)V",
+                    shift = At.Shift.AFTER
+            )
+    )
+    private void stationapi_onTake(ItemStack stack, CallbackInfo ci) {
+        RecipeInit.handlePainter(stack, input);
+    }
+}
