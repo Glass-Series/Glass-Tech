7228a2500559ce6520293cafc8d6685c8f81fd6f
calmilamsy <bumbill00@gmail.com>
2026-04-15 16:45:34 +0100

    Oops

--- src/main/java/net/glasslauncher/mods/glasstech/blocks/GTTemplateBlock.java
+++ src/main/java/net/glasslauncher/mods/glasstech/blocks/GTTemplateBlock.java
@@ -1,6 +1,5 @@
 package net.glasslauncher.mods.glasstech.blocks;
 
-import net.glasslauncher.mods.glasstech.GTTextureInit;
 import net.minecraft.block.material.Material;
 import net.minecraft.sound.BlockSoundGroup;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlas;
@@ -8,7 +7,7 @@ import net.modificationstation.stationapi.api.client.texture.atlas.Atlases;
 import net.modificationstation.stationapi.api.template.block.TemplateBlock;
 import net.modificationstation.stationapi.api.util.Identifier;
 
-public class GTTemplateBlock extends TemplateBlock implements GTTextureInit {
+public class GTTemplateBlock extends TemplateBlock {
     public final Identifier identifier;
 
     public GTTemplateBlock(Identifier identifier, Material material, BlockSoundGroup soundGroup) {
