4e810fd3d1f24bddffa4b2807dab43e1a85da91a
calmilamsy <bumbill00@gmail.com>
2026-05-18 21:00:56 +0100

    Update GTDynamiteBlock.java

--- src/main/java/net/glasslauncher/mods/glasstech/blocks/GTDynamiteBlock.java
+++ src/main/java/net/glasslauncher/mods/glasstech/blocks/GTDynamiteBlock.java
@@ -1,11 +1,20 @@
 package net.glasslauncher.mods.glasstech.blocks;
 
+import net.glasslauncher.mods.glasstech.entity.GTExplosion;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import net.modificationstation.stationapi.api.util.Identifier;
 
 public class GTDynamiteBlock extends GTTemplateBlock {
     public GTDynamiteBlock(Identifier identifier) {
         super(identifier, Material.TNT, Block.DEFAULT_SOUND_GROUP);
+    }
+
+    public void explode(World world, int x, int y, int z) {
+        Explosion explosion = new GTExplosion(world, null, x, y, z, 1);
+        explosion.explode();
+        explosion.playExplosionSound(true);
     }
 }
