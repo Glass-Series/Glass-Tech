060c5833ff2a70627bd47f7ea2e1ed87b0434567
calmilamsy <bumbill00@gmail.com>
2026-08-12 00:20:18 +0100

    Add missing armor textures

--- src/main/java/net/glasslauncher/mods/glasstech/item/CompositeChestplate.java
+++ src/main/java/net/glasslauncher/mods/glasstech/item/CompositeChestplate.java
@@ -1,11 +1,21 @@
 package net.glasslauncher.mods.glasstech.item;
 
+import net.minecraft.item.ArmorItem;
+import net.modificationstation.stationapi.api.client.item.ArmorTextureProvider;
 import net.modificationstation.stationapi.api.template.item.TemplateArmorItem;
 import net.modificationstation.stationapi.api.util.Identifier;
 
-public class CompositeChestplate extends TemplateArmorItem {
+public class CompositeChestplate extends TemplateArmorItem implements ArmorTextureProvider {
+    private final Identifier identifier;
+
     public CompositeChestplate(Identifier identifier, int type, int textureIndex, int equipmentSlot) {
         super(identifier, type, textureIndex, equipmentSlot);
+        this.identifier = identifier;
         setMaxDamage(256);
+    }
+
+    @Override
+    public Identifier getTexture(ArmorItem armor) {
+        return identifier;
     }
 }
